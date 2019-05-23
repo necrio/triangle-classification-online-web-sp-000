eccfaec77957ca090361b4bf7d95baa17f5505d7
Keith Isham <kisham8899@gmail.com>
2019-05-23 21:31:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,25 +10,25 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-    # else
-    #   if (@side_1 == @side_2) && (@side_2 == @side_3)
-    #     :equilateral
-    #   elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-    #     :isosceles
-    #   elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-    #     :scalene
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
       end
     end
 
 
 
-  def kosher_triangle
-    side_1.positive? && side_2.positive? && side_3.positive?
-  end
-
-  def kosher_triangle2
-    side_1 + side_2 > side_3 && side_1 + side_3 > side_2 && side_2 + side_3 > side_1
-  end
+  # def kosher_triangle
+  #   side_1.positive? && side_2.positive? && side_3.positive?
+  # end
+  # 
+  # def kosher_triangle2
+  #   side_1 + side_2 > side_3 && side_1 + side_3 > side_2 && side_2 + side_3 > side_1
+  # end
 
 
 
