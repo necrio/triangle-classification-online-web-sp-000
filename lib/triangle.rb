d072bf3ba445bf3ba3182965c5af8561d8801ede
Keith Isham <kisham8899@gmail.com>
2019-05-23 21:28:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,17 +10,27 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
-        :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-        :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-        :scalene
+    # else
+    #   if (@side_1 == @side_2) && (@side_2 == @side_3)
+    #     :equilateral
+    #   elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+    #     :isosceles
+    #   elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+    #     :scalene
       end
     end
 
   end
+
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end
+
+  
 
 end
 
